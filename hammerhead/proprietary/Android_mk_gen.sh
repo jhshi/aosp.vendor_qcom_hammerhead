244ef71eb7a99f95daa45e21f536ed8fb33f35b3
Jinghao Shi <jinghaos@buffalo.edu>
2014-11-04 10:39:10 -0500

    init

---
+++ hammerhead/proprietary/Android_mk_gen.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+for file in $(find . -name "*.apk"); do
+
+    apk_name=$(basename $file)
+    module_name=${apk_name//\.apk/}
+
+	cat <<- EOF
+	include \$(CLEAR_VARS)
+	LOCAL_MODULE := $module_name
+	LOCAL_SRC_FILES := $file
+    LOCAL_MODULE_PATH := $(TARGET_OUT_APPS)
+	LOCAL_MODULE_CLASS := APPS
+	LOCAL_MODULE_OWNER := qcom
+	LOCAL_MODULE_TAGS := optional
+	LOCAL_CERTIFICATE := PRESIGNED
+	LOCAL_MODULE_SUFFIX := \$(COMMON_ANDROID_PACKAGE_SUFFIX)
+	include \$(BUILD_PREBUILT)
+
+	EOF
+
+done
+
