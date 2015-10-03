2edc527a579c0d63d15c56547a2ad82259df7da3
Jinghao Shi <jinghaos@buffalo.edu>
2015-10-03 11:52:13 -0400

    fix librpmb.so and libssd.so

--- hammerhead/device-partial.mk
+++ hammerhead/device-partial.mk
@@ -170,4 +170,6 @@ PRODUCT_COPY_FILES := \
     vendor/qcom/hammerhead/proprietary/libsensor_user_cal.so:system/vendor/lib/libsensor_user_cal.so:qcom \
     vendor/qcom/hammerhead/proprietary/libtime_genoff.so:system/vendor/lib/libtime_genoff.so:qcom \
     vendor/qcom/hammerhead/proprietary/libTimeService.so:system/vendor/lib/libTimeService.so:qcom \
+    vendor/qcom/hammerhead/proprietary/librpmb.so:system/lib/librpmb.so:qcom \
+    vendor/qcom/hammerhead/proprietary/libssd.so:system/lib/libssd.so:qcom \
 
